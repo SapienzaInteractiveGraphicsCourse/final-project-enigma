66d0643271f2827bf1273bd55fabbd65cb1d6db7
Marco Marcovecchio <marcoldmark@gmail.com>
2026-06-27 15:51:56 +0200

    0.4.1
    more materials and ui

--- ui.js
+++ ui.js
@@ -2,11 +2,12 @@ import { setMaterialColor } from './color.js';
 
 const BodyColorPicker = document.getElementById('bodyColorPicker');
 const CaliperColorPicker = document.getElementById('caliperColorPicker');
+const RimColorPicker = document.getElementById('rimColorPicker');
+const SeatColorPicker = document.getElementById('seatColorPicker');
 
 if(BodyColorPicker) {
     BodyColorPicker.addEventListener('input', (evento) => {
         const newColor = evento.target.value;
-        console.log("new color:", newColor);
         setMaterialColor('body_paint', newColor);
     });
 }
@@ -14,7 +15,20 @@ if(BodyColorPicker) {
 if(CaliperColorPicker) {
     CaliperColorPicker.addEventListener('input', (evento) => {
         const newColor = evento.target.value;
-        console.log("new color:", newColor);
         setMaterialColor('caliper', newColor);
+    });
+}
+
+if(RimColorPicker) {
+    RimColorPicker.addEventListener('input', (evento) => {
+        const newColor = evento.target.value;
+        setMaterialColor('rim', newColor);
+    });
+}
+
+if(SeatColorPicker) {
+    SeatColorPicker.addEventListener('input', (evento) => {
+        const newColor = evento.target.value;
+        setMaterialColor('fabric', newColor);
     });
 }
