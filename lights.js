f062dd405cf23e9b8e47a77c4bff2d3ef1066bca
mariagiuliaruga <ruga.2051428@studenti.uniroma1.it>
2026-06-28 09:28:39 +0200

    0.6.1

--- lights.js
+++ lights.js
@@ -16,7 +16,7 @@ export function setupEnvironmentLights(scene) {
     frontLight.shadow.camera.top = 10;
     frontLight.shadow.camera.bottom = -10;
     frontLight.shadow.bias = -0.001; 
-    frontLight.shadow.normalBias = 0.05;
+    frontLight.shadow.normalBias = 0.05; 
     scene.add(frontLight);
 
     const backLight = new THREE.DirectionalLight(0xffffff, 3.5);
@@ -33,6 +33,36 @@ export function setupEnvironmentLights(scene) {
     backLight.shadow.bias = -0.001; 
     backLight.shadow.normalBias = 0.05;
     scene.add(backLight);
+
+    const leftLight = new THREE.DirectionalLight(0xffffff, 2.5);
+    leftLight.position.set(-10, 6, 0);
+    leftLight.castShadow = true;
+    leftLight.shadow.mapSize.width = 2048;
+    leftLight.shadow.mapSize.height = 2048;
+    leftLight.shadow.camera.near = 0.5;
+    leftLight.shadow.camera.far = 25;
+    leftLight.shadow.camera.left = -10;
+    leftLight.shadow.camera.right = 10;
+    leftLight.shadow.camera.top = 10;
+    leftLight.shadow.camera.bottom = -10;
+    leftLight.shadow.bias = -0.001;
+    leftLight.shadow.normalBias = 0.05;
+    scene.add(leftLight);
+
+    const rightLight = new THREE.DirectionalLight(0xffffff, 2.5);
+    rightLight.position.set(10, 6, 0);
+    rightLight.castShadow = true;
+    rightLight.shadow.mapSize.width = 2048;
+    rightLight.shadow.mapSize.height = 2048;
+    rightLight.shadow.camera.near = 0.5;
+    rightLight.shadow.camera.far = 25;
+    rightLight.shadow.camera.left = -10;
+    rightLight.shadow.camera.right = 10;
+    rightLight.shadow.camera.top = 10;
+    rightLight.shadow.camera.bottom = -10;
+    rightLight.shadow.bias = -0.001;
+    rightLight.shadow.normalBias = 0.05;
+    scene.add(rightLight);
 }
 
 export function toggleCarLight(lightObject, isVisible) {
