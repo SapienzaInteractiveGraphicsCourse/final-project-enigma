8572228656d1ec4b04118fbaa3760d2ba8d4dae0
mariagiuliaruga <ruga.2051428@studenti.uniroma1.it>
2026-06-30 10:22:34 +0200

    1.0.2.2
    blinkers

--- code/script/sound.js
+++ code/script/sound.js
@@ -2,11 +2,14 @@ const blinkSound = new Audio('../../src/audio/blinker.wav');
 blinkSound.volume = 1.0; 
 
 export function playTurnSignalSound() {
-    blinkSound.currentTime = 0; 
-    blinkSound.play().catch(e => console.warn("Audio blocked:", e));
+    if (blinkSound.paused) {
+        blinkSound.currentTime = 0; 
+        blinkSound.play().catch(e => console.warn("Audio blocked:", e));
+    }
 }
 
 export function stopTurnSignalSound() {
     blinkSound.pause();
     blinkSound.currentTime = 0;
 }
+
